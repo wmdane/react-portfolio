45ff75ac71042261a2cdab1ccad0c25b8f1ff083
Will Dane <enadlliw@gmail.com>
2021-01-14 17:50:58 -0500

    updated App.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,11 +4,17 @@ import Photo from "../images/profile-pic.jpg";
 function About() {
   return (
     <div>
-      <img alt="William Dane" src= {Photo} id="profile-pic" />
+      <img alt="William Dane" src={Photo} id="profile-pic" />
       <h1>About Me</h1>
-      <p></p>
+      <p>
+        My name is William Dane. I am a full-stack web developer currently living in Charlotte,
+        North Carolina. I am proficient in HTML, CSS, JavaScript, APIs, Express, Node, MySQL,
+        MongoDB, and React. I received my Bachelor's Degree from the University of North Carolina at
+        Greensboro in 2016. Thank you for taking the time to visit my portfolio. Please visit the
+        Contact tab to see how to contact me regarding any business inquiries or recruitment offers.
+      </p>
     </div>
-  )
+  );
 }
 
 export default About;
